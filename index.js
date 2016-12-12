1bd9a9ba36c663e58fdb4699512e607c2de79088
Yotam <yotam.rt@gmail.com>
2016-12-12 11:06:53 +0200

    Added new features: 1) Connect farthest nodes option 2) Auto connect nodes is no longer default 3) Added events bindings 4) Added extended options

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+
 import ngraph from 'ngraph.graph'
+import getGraphDiameter from './bfs'
 import { diff } from 'deep-diff'
 import { KINDS } from './constants'
 
@@ -7,12 +9,21 @@ class Graph extends Component {
   static defaultProps = {
     renderer: null,
     layout: null,
+    autoConnectNodes: false,
     data: {
       relations: [],
       tables: [],
     },
     linkedOnly: false,
-    maxSteps: 100,
+    settings: {
+      maxSteps: 100,
+    },
+    eventBinding: {
+
+    },
+    extendedOptions: {
+
+    },
   }
 
   constructor(props){
@@ -24,12 +35,12 @@ class Graph extends Component {
     }
   }
 
-  prepareGraph(graph, graphDiff=[], graphData, linkedOnly){
+  prepareGraph(graph, graphDiff=[], graphData, linkedOnly, autoConnectNodes, connectFarthestNodes){
     let nodesToBeAdded = [];
     let dimentionId=1;
 
     graphDiff.forEach(diff=>{
-      if (diff.path[0]==='tables'||diff.path[0]==='relations'){
+      if (diff.path && (diff.path[0]==='tables'||diff.path[0]==='relations')){
         if (diff.item.kind===KINDS.NEW){
           if (diff.path[0]==='tables'){
             const table = diff.item.rhs
@@ -38,21 +49,44 @@ class Graph extends Component {
             }
           }else if (diff.path[0]==='relations'){
             const relation = Object.assign({},diff.item.rhs, {type:'dimention'})
-            graph.addNode('dimention' + dimentionId, relation);
+
+            if (autoConnectNodes) {
+              graph.addNode('dimention' + dimentionId, relation);
+            }
 
             for (var index = 0; index < relation.connections.length; index++) {
               if (linkedOnly){
                 const node = graph.getNode(relation.connections[index].TableName)
-                if (node===undefined){
+                if (!autoConnectNodes && node===undefined){
                   nodesToBeAdded.push(relation.connections[index].TableName)
                 }
               }
-              graph.addLink(
-                relation.connections[index].TableName,
-                'dimention' + dimentionId,
-                [relation.connections[index],{TableName:'dimention' + dimentionId}]
-              );
+
+              if (autoConnectNodes) {
+                graph.addLink(
+                  relation.connections[index].TableName,
+                  'dimention' + dimentionId,
+                  [relation.connections[index],{TableName:'dimention' + dimentionId}]
+                );
+              }
             }
+
+            if (!autoConnectNodes) {
+              relation.connections.forEach((connection)=>{
+                relation.connections.forEach((connection2)=>{
+                  if (connection2.TableName!==connection.TableName &&
+                      !graph.getLink(connection2.TableName, connection.TableName) &&
+                      !graph.getLink(connection.TableName, connection2.TableName)){
+                    graph.addLink(
+                      connection.TableName,
+                      connection2.TableName,
+                      [connection,{TableName:connection2.TableName}]
+                    );
+                  }
+                });
+              });
+            }
+
             dimentionId++;
           }
         } else if (diff.item.kind===KINDS.DELETE){
@@ -61,29 +95,35 @@ class Graph extends Component {
       }
     })
 
+    if (connectFarthestNodes) {
+      this.connectFarthestNodesOfGraph(graph);
+    }
     graphDiff
     .filter(diff=>{
-      return diff.path[0]==='tables' && nodesToBeAdded.includes(diff.item.rhs.name)
+      return diff.path && diff.path[0]==='tables' && nodesToBeAdded.includes(diff.item.rhs.name)
     })
     .forEach(diff=>{
       const table = diff.item.rhs
       graph.addNode(table.name, table)
-    })
+    });
+
   }
 
   redrawGraph(){
-    const {renderer, maxSteps, linkedOnly} = this.props
-    const {graphDiff} = this.state
+    const {renderer, linkedOnly, extendedOptions, autoConnectNodes, settings, layoutSettings, connectFarthestNodes, eventBinding} = this.props;
+    const {graphDiff} = this.state;
 
     if (renderer && graphDiff && graphDiff.length>0){
-      this.prepareGraph(this.graph, graphDiff, this.props.data, linkedOnly)
+      this.prepareGraph(this.graph, graphDiff, this.props.data, linkedOnly, autoConnectNodes, connectFarthestNodes);
+      settings.container = this.graphContainer;
+
       let graphics = this.props.renderer(
         this.graph,
-        {
-          container: this.refs.graphContainer,
-          maxSteps,
-        },
+        settings,
+        layoutSettings,
         ngraph,
+        eventBinding,
+        extendedOptions
         // doubleClickHandler
       )
       // console.log(graphics);
@@ -99,25 +139,53 @@ class Graph extends Component {
   componentWillReceiveProps(nextProps){
   // componentWillUpdate(nextProps){
     const graphDiff = diff(this.props.data, nextProps.data)
-    console.log(graphDiff);
     this.setState({
       graphDiff
     })
   }
 
+  connectFarthestNodesOfGraph(graph) {
+    let adjList = {};
+
+    graph.forEachNode((node) => {
+      if (!adjList[node.id]) {
+        adjList[node.id] = [];
+      }
+
+      node.links.forEach((link) => {
+        if (link.toId === node.id) {
+          adjList[node.id].push(link.fromId);
+        } else {
+          adjList[node.id].push(link.toId);
+        }
+      });
+    });
+
+    let graphDiameter = getGraphDiameter(adjList);
+
+    if (!graph.hasLink(graphDiameter.nodeA, graphDiameter.nodeB) &&
+        !graph.hasLink(graphDiameter.nodeB, graphDiameter.nodeA)) {
+          graph.addLink(
+            graphDiameter.nodeA,
+            graphDiameter.nodeB,
+            [graphDiameter.nodeA,{TableName:graphDiameter.nodeB}]
+          );
+        }
+  }
+
   shouldComponentUpdate(nextProps){
     const graphDiff = diff(this.props.data, nextProps.data)
-    console.log('diff: ', (graphDiff && graphDiff.length>0) ? true:false);
     return (graphDiff && graphDiff.length>0) ? true:false
   }
 
   render(){
-    const {className} = this.props
+    const {className, extendedOptions, data} = this.props
+
     this.redrawGraph()
     return(
       <div
         className={className}
-        ref="graphContainer"
+        ref={(element)=>{this.graphContainer = element;}}
       />
     )
   }
